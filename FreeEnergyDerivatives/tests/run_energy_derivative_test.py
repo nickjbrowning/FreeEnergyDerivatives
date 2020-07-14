87812d49e6538532a6893bb2679ab39cf6b597b4
nickbrowning <nickjbrowning@gmail.com>
2020-07-14 13:00:41 +0200

    more mods

--- FreeEnergyDerivatives/tests/run_energy_derivative_test.py
+++ FreeEnergyDerivatives/tests/run_energy_derivative_test.py
@@ -20,6 +20,7 @@ from openmmtools.alchemy import  *
 
 from lib import solvation_potentials as sp
 from openmmtools.testsystems import TestSystem
+from __builtin__ import False, True
 
 
 class CustomSystem(TestSystem):
@@ -41,13 +42,17 @@ class CustomSystem(TestSystem):
         force.setNonbondedMethod(openmm.NonbondedForce.NoCutoff)
 
         # Create positions.
-        positions = unit.Quantity(np.zeros([2, 3], np.float32), unit.angstrom)
+        positions = unit.Quantity(np.zeros([3, 3], np.float32), unit.angstrom)
         # Move the second particle along the x axis to be at the potential minimum.
         positions[1, 0] = 2.0 ** (1.0 / 6.0) * sigma
+        positions[2, 0] = 4.0 ** (1.0 / 6.0) * sigma
 
         system.addParticle(mass)
         force.addParticle(charge, sigma, epsilon)
 
+        system.addParticle(mass)
+        force.addParticle(charge, sigma, epsilon)
+        
         system.addParticle(mass)
         force.addParticle(charge, sigma, epsilon)
 
@@ -65,37 +70,46 @@ class CustomSystem(TestSystem):
         topology.addAtom('Ar', element, residue)
         residue = topology.addResidue('Ar', chain)
         topology.addAtom('Ar', element, residue)
+        residue = topology.addResidue('Ar', chain)
+        topology.addAtom('Ar', element, residue)
         self.topology = topology
 
 
 test = CustomSystem()
 
+compute_response = False
+disable_longrange = True
 system, positions, topology = test.system, test.positions, test.topology
 
-alchemical_system = sp.create_alchemical_system(system, solute_indicies=[0, 1], softcore_beta=0.0, softcore_m=1.0, compute_solvation_response=True)
+alchemical_system = sp.create_alchemical_system(system, solute_indicies=[0, 1], softcore_beta=0.0, softcore_m=1.0,
+                                                disable_alchemical_dispersion_correction=disable_longrange, compute_solvation_response=compute_response)
+
 integrator = LangevinIntegrator(298.15 * unit.kelvin, 1.0 / unit.picoseconds, 0.002 * unit.picoseconds)
 
 context = Context(alchemical_system, integrator)
 
-positions = unit.Quantity(np.zeros([2, 3], np.float32), unit.angstrom)
+positions = unit.Quantity(np.zeros([3, 3], np.float32), unit.angstrom)
 
 for distance in np.linspace(2.5, 5.0, 10):
-    positions[0, 0] = distance * unit.angstroms
+    positions[1, 1] = distance * unit.angstroms
     
     context.setPositions(positions)
     
-    state = context.getState(getEnergy, getEnergyParameterDerivatives=True, group=set([0]))
-    
-    state = simulation.context.getState(getEnergy=True, getParameterDerivatives=True)
+    state = context.getState(getEnergy=True, getParameterDerivatives=True, groups=set([0]))
 
     energy_derivs = state.getEnergyParameterDerivatives()
     
-    state_deriv_electrostatics = simulation.context.getState(getEnergy=True, groups=set([1]))
-    state_deriv_sterics = simulation.context.getState(getEnergy=True, groups=set([2]))
-            
-    energy_deriv_electrostatics = energy_derivs['lambda_electrostatics']
-    energy_deriv_sterics = energy_derivs['lambda_sterics']
+    print (state.getPotentialEnergy())
     
-    print ("Electrostatics Diff:", energy_deriv_electrostatics, state_deriv_electrostatics.getPotentialEnergy()._value)
-    print ("Sterics Diff:", energy_deriv_sterics, state_deriv_sterics.getPotentialEnergy()._value)
+    print (energy_derivs)
+    
+    if (compute_response):
+        state_deriv_electrostatics = context.getState(getEnergy=True, groups=set([1]))
+        energy_deriv_electrostatics = energy_derivs['lambda_electrostatics']
+        
+        print ("Electrostatics Diff:", energy_deriv_electrostatics, state_deriv_electrostatics.getPotentialEnergy())
+        
+        state_deriv_sterics = context.getState(getEnergy=True, groups=set([2]))
+        energy_deriv_sterics = energy_derivs['lambda_sterics']
+        print ("Sterics Diff:", energy_deriv_sterics, state_deriv_sterics.getPotentialEnergy())
     
