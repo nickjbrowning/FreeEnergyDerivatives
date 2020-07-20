e847e4850717f619f7b0e19bab002bc43252c853
nick <nick@nick-UX303UB>
2020-07-20 18:41:19 +0200

    asd

---
+++ FreeEnergyDerivatives/tools/bin_dihedrals.py
@@ -0,0 +1,50 @@
+import netCDF4
+import argparse
+
+parser = argparse.ArgumentParser()
+parser.add_argument('-netcdf', type=str, default=None)
+parser.add_argument('-indexes', type=int, nargs='+', default=None)
+
+
+def calc_dihedral(coordinates):
+   
+    p0 = coordinates[0]
+    p1 = coordinates[1]
+    p2 = coordinates[2]
+    p3 = coordinates[3]
+
+    b0 = -1.0 * (b - a)
+    b1 = c - b
+    b2 = d - c
+
+    b1 /= np.linalg.norm(b1)
+
+    v = b0 - np.dot(b0, b1) * b1
+    w = b2 - np.dot(b2, b1) * b1
+
+    x = np.dot(v, w)
+    y = np.dot(np.cross(b1, v), w)
+    return np.degrees(np.arctan2(y, x))
+
+
+args = parser.parse_args()
+
+ncin = Dataset(args.netcdf, 'r', format='NETCDF5')
+
+atom_indexes = np.array(args.indexes)
+
+coordinates = ncin.variables['coordinates'][:]
+
+dihedrals = np.zeros(coordinates.shape[0])
+
+for i in coordinates.shape[0]:
+    dihedral_coords = coordinates[i][atom_indexes, :]
+    
+    dihedral = calc_dihedral(dihedral_coords)
+    
+    dihedrals[i] = dihedral
+    
+import matplotlib.pyplot as plt
+plt.hist(dihedrals, bins=50)
+plt.show()
+    
