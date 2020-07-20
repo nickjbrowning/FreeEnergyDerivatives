4bf7075b06e6f3a9bd969d98cc668c28bb1edc96
nickbrowning <nickjbrowning@gmail.com>
2020-07-20 14:45:41 +0200

    asd

--- FreeEnergyDerivatives/tools/sample_configurations.py
+++ FreeEnergyDerivatives/tools/sample_configurations.py
@@ -21,11 +21,28 @@ parser.add_argument('-pdb', type=str)
 parser.add_argument('-solvate', type=bool, default=True)
 parser.add_argument('-nsamples', type=int, default=250)  
 parser.add_argument('-nsample_steps', type=int, default=10000)  # 20ps using 2fs timestep
+parser.add_argument('-solute_indexes', type=int, nargs='+', default=None)
 args = parser.parse_args()
 
 platform = openmm.Platform.getPlatformByName('CUDA')
 platform.setPropertyDefaultValue('Precision', 'mixed')
 
+
+def collect_solute_indexes(topology):
+    soluteIndices = []
+    for res in topology.residues():
+        resname = res.name.upper()
+        if (resname != 'HOH' and resname != 'WAT'and resname != 'CL'and resname != 'NA'):
+            for atom in res.atoms():
+                soluteIndices.append(atom.index)
+    return soluteIndices
+
+
+if (args.solute_indexes == None):
+    solute_indexes = collect_solute_indexes(modeller.topology)
+else:
+    solute_indexes = np.array(args.solute_indexes)
+    
 '''
 ---SYSTEM PREPARATION---
     setup AM1-BCC charges for the solute, add solvent, set non-bonded method etc
@@ -72,7 +89,7 @@ simulation.step(100000)
 for iteration in range(args.nsamples):
     
     state = simulation.context.getState(getPositions=True)
-    PDBFile.writeFile(ligand_pdb.topology, state.getPositions(), file=open("sample_" + str(iteration) + ".pdb", "w"))
+    PDBFile.writeFile(ligand_pdb.topology, state.getPositions()[solute_indexes, :], file=open("sample_" + str(iteration) + ".pdb", "w"))
     
     simulation.step(args.nsample_steps)
 
