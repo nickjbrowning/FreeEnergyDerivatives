f1656d65ca154f66463ff4701374d4cfda775e38
nickbrowning <nickjbrowning@gmail.com>
2020-07-23 11:16:54 +0200

    asd

--- FreeEnergyDerivatives/lib/utils.py
+++ FreeEnergyDerivatives/lib/utils.py
@@ -55,7 +55,7 @@ def collect_solute_indexes(topology):
 
 
 def strip_netcdf(incdf, outcdf, atom_indexes):
-    with nc.Dataset(incdf, "r") as src, nc.Dataset(outcdf, "w") as dst:
+    with nc.Dataset(incdf, "r") as src, nc.Dataset(outcdf, "w", format="NETCDF4") as dst:
         # copy attributes
         for name in src.ncattrs():
             dst.setncattr(name, src.getncattr(name))
