08e7d891f429086cec7fdb7661f044304fc2b366
Nicholas Browning <nickjbrowning@gmail.com>
2020-08-07 22:22:11 +0200

    added metadynamics + qml code

--- FreeEnergyDerivatives/tools/run_metadynamics_qml.py
+++ FreeEnergyDerivatives/tools/run_metadynamics_qml.py
@@ -3,7 +3,7 @@ from simtk.openmm import *
 from simtk import unit
 from simtk.openmm import app
 from simtk.openmm.app import PDBFile, Modeller, PDBFile
-from parmed.openmm.reporters import NetCDFReporter  
+from parmed.openmm.reporters import  NetCDFReporter
 from openmmtools import alchemy
 import numpy as np
 from time import time
@@ -19,30 +19,58 @@ from openmmtools import forces
 from openmmtools.alchemy import  *
 
 from lib import solvation_potentials as sp
-from pathlib import Path
+
 from lib import utils
 
+from openmmplumed  import *
 from openmmqml import QMLForce
 
-platform = openmm.Platform.getPlatformByName('CUDA')
-platform.setPropertyDefaultValue('Precision', 'mixed')
+import argparse
 
-ligand_pdb = PDBFile('alanine_dipeptide.pdb')
+platform = openmm.Platform.getPlatformByName('Reference')
+# platform.setPropertyDefaultValue('Precision', 'mixed')
+
+parser = argparse.ArgumentParser()
+
+parser.add_argument('-pdb', type=str, help='PDB Structure File')
+parser.add_argument('-script', type=str, help='Plumed Input File')
+
+args = parser.parse_args()
+
+script = open(args.script, 'r').read()
+
+print ("--Plumed Script--")
+print (script)
+
+'''
+---SYSTEM PREPARATION---
+    setup AM1-BCC charges for the solute, add solvent, set non-bonded method etc
+'''
+
+ligand_pdb = PDBFile(args.pdb)
+
 forcefield = ForceField('amber/protein.ff14SB.xml', 'amber/tip3p_standard.xml', 'amber/tip3p_HFE_multivalent.xml')
 
 modeller = Modeller(ligand_pdb.topology, ligand_pdb.positions)
 
 system = forcefield.createSystem(modeller.topology, nonbondedMethod=CutoffNonPeriodic,
-        nonbondedCutoff=10.0 * unit.angstroms, constraints=HBonds, switchDistance=9.0 * unit.angstroms)
+        nonbondedCutoff=10.0 * unit.angstroms, constraints=HBonds)
 
 solute_indexes = utils.collect_solute_indexes(modeller.topology)
+
+'''
+---FINISHED SYSTEM PREPARATION---
+'''
+    
+integrator = LangevinIntegrator(298.15 * unit.kelvin, 1.0 / unit.picoseconds, 0.002 * unit.picoseconds)
+
+system.addForce(PlumedForce(script))
 
 training_atoms = np.fromfile('atoms.bin', dtype=np.int32).tolist()
 training_charges = np.fromfile('charges.bin', dtype=np.int32).tolist()
 training_coefficients = np.fromfile('coefficients.bin', dtype=np.double).tolist()
 training_reps = np.fromfile('training_reps.bin', dtype=np.double).tolist()
 
-print (training_atoms)
 elements2charge = {'H': 1, 'C': 6, 'N': 7, 'O':8}
 charges = []
 for atom in modeller.topology.atoms():
@@ -75,9 +103,6 @@ qmlforce.setSigma(21.2)
 
 system.addForce(qmlforce)
 
-# system.addForce(MonteCarloBarostat(1 * unit.bar, 298.15 * unit.kelvin))
-integrator = LangevinIntegrator(298.15 * unit.kelvin, 1.0 / unit.picoseconds, 0.002 * unit.picoseconds)
-
 simulation = app.Simulation(modeller.topology, system, integrator, platform)
 simulation.context.setPositions(modeller.positions)
 
@@ -86,10 +111,11 @@ simulation.minimizeEnergy()
 state = simulation.context.getState(getPositions=True)
 PDBFile.writeFile(modeller.topology, state.getPositions(), file=open("equil.pdb", "w"))
 
-simulation.reporters.append(StateDataReporter('data.txt', 100, step=True, potentialEnergy=True, temperature=True, density=True , volume=True))
-simulation.reporters.append(NetCDFReporter('output.nc', 100))
+simulation.reporters.append(StateDataReporter('data.txt', 1500, step=True, potentialEnergy=True, temperature=True, density=True , volume=True))
+simulation.reporters.append(NetCDFReporter('output.nc', 1500))
 
 start = time()
-simulation.step(10000)
+simulation.step(7500000)
 end = time()
 print (end - start)
+
